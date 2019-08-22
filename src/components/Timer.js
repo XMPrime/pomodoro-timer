878a8890088b723368e306014d366f16cf9eecb7
Jason Chen <jason.l.chen@gmail.com>
2019-08-21 20:44:53 -0700

    it works!

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import { switchMode, workTimeTick, breakTimeTick, reset } from "../actions/actions";
+import { switchMode, workTimeTick, breakTimeTick, reset, startStopToggle } from "../actions/actions";
 
 class Timer extends Component {
   switchMode = () => {
@@ -16,39 +16,89 @@ class Timer extends Component {
     this.props.reset();
   };
 
+  displayTimeLeft(seconds) {
+    let secondsLeft = seconds;
+    const minutes = Math.floor(secondsLeft / 60);
+    secondsLeft = Math.round(secondsLeft % 60);
+    let countdown;
+
+    if (this.props.isTicking) {
+      console.log("tick");
+      countdown = setInterval(() => {
+        clearInterval(countdown);
+        this.props.workMode ? this.workTimeTick() : this.breakTimeTick();
+      }, 1000);
+    }
+
+    const display = `${minutes < 10 ? "0" : ""}${minutes}:${secondsLeft < 10 ? "0" : ""}${secondsLeft}`;
+
+    if (display === "00:00") {
+      clearInterval(countdown);
+      this.switchMode();
+      this.reset();
+
+      return;
+    }
+
+    return display;
+  }
+
   render() {
-    const displayTimeLeft = seconds => {
-      let secondsLeft = seconds;
-      const minutes = Math.floor(seconds / 60);
-      secondsLeft = Math.round(secondsLeft % 60);
+    // const displayTimeLeft = seconds => {
+    //   let secondsLeft = seconds;
+    //   const minutes = Math.floor(seconds / 60);
+    //   secondsLeft = Math.round(secondsLeft % 60);
 
-      if (this.props.isTicking) {
-        let countdown = setInterval(() => {
-          clearInterval(countdown);
-          if (this.props.isTicking) {
-            if (this.props.workTimerSeconds <= 0 || this.props.workTimerSeconds <= 0) {
-              clearInterval(countdown);
-              this.switchMode();
-              this.reset();
-              return;
-            }
-          } else {
-            clearInterval(countdown);
-            return;
-          }
-          this.props.workMode ? this.workTimeTick() : this.breakTimeTick();
-        }, 200);
-      }
+    //   const countdown = setInterval(() => {
+    //     if (this.props.isTicking) {
+    //       if (this.props.workTimerSeconds <= 0 || this.props.breakTimerSeconds <= 0) {
+    //         this.switchMode();
+    //         this.reset();
+    //         this.props.startStopToggle();
 
-      const display = `${minutes < 10 ? "0" : ""}${minutes}:${secondsLeft < 10 ? "0" : ""}${secondsLeft}`;
-      return display;
-    };
+    //         return;
+    //       }
+    //     } else {
+    //       clearInterval(countdown);
+    //       return;
+    //     }
+    //     this.props.workMode ? this.workTimeTick() : this.breakTimeTick();
+    //     clearInterval(countdown);
+    //   }, 1000);
+
+    //   const display = `${minutes < 10 ? "0" : ""}${minutes}:${secondsLeft < 10 ? "0" : ""}${secondsLeft}`;
+
+    //   return display;
+    // };
+    // let countdown;
+
+    // function timer(seconds, isTicking, workTimeTick, breakTimeTick) {
+    //   clearInterval(countdown); // clear any existing timers
+
+    //   countdown = setInterval(() => {
+    //     if (isTicking) {
+    //       if (seconds < 0) {
+    //         clearInterval(countdown);
+    //         this.switchMode();
+    //         this.reset();
+    //         return;
+    //       }
+    //       console.log(this);
+    //       isTicking ? workTimeTick() : breakTimeTick();
+    //     } else {
+    //       clearInterval(countdown);
+    //       return;
+    //     }
+    //   }, 1000);
+    // }
+
+    // timer(this.props.workTimerSeconds, this.props.isTicking, this.workTimeTick, this.breakTimeTick);
 
     return (
       <div>
         <h1>{this.props.workMode ? "Time until break!" : "Time until work!"}</h1>
         <h1 className={this.props.workMode ? "workTimer" : "breakTimer"}>
-          {this.props.workMode ? displayTimeLeft(this.props.workTimerSeconds) : displayTimeLeft(this.props.breakTimerSeconds)}
+          {this.props.workMode ? this.displayTimeLeft(this.props.workTimerSeconds) : this.displayTimeLeft(this.props.breakTimerSeconds)}
         </h1>
       </div>
     );
@@ -70,6 +120,7 @@ const mapDispatchToProps = {
   switchMode,
   workTimeTick,
   breakTimeTick,
+  startStopToggle,
   reset
 };
 
